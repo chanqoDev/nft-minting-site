abac2ef76a619df66b45bace21375ffae822fade
Christian E <christianelena92@gmail.com>
2022-08-13 18:42:31 -0500

    add App structure and styling

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="mintingSection">
+          <div className="left">
+            <video height="400" width="400" autoPlay muted playsInline loop>
+              <source
+                src="https://cdn-std.droplr.net/files/acc_990213/d9ucir"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="right">
+            <h2> ADDIDAS: INTO THE METAVERSE </h2>
+            <div>12 minted / 200</div>
+            <div>CONNECT WALLET</div>
+          </div>
+        </div>
+        <div className="footer">MINTING NOW!</div>
+      </div>
     </div>
   );
 }
