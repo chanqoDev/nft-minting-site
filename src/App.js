4297f218647f72d1548d1e62e58499a1e996ad28
Christian E <christianelena92@gmail.com>
2022-08-15 22:12:00 -0500

    add contract, json with older -v of Moralis in the yarn file, ADD IBA array of objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useMoralis } from 'react-moralis';
+import { useState, useEffect } from 'react';
+import abi from './contract/contract.json';
 
 function App() {
+  const [totalSupply, setTotalSupply] = useState(0);
+  const {
+    Moralis,
+    enableWeb3,
+    authenticate,
+    isAuthenticated,
+    isAuthenticating,
+    user,
+    account,
+    logout,
+  } = useMoralis();
+
+  const logOut = async () => {
+    await logout();
+  };
+
+  useEffect(() => {
+    const getSupply = async () => {
+      if (isAuthenticated) {
+        await enableWeb3();
+        getTotalSupply();
+      }
+    };
+    getSupply();
+  }, [isAuthenticated]);
+
+  const mint = async () => {
+    const sendOptions = {
+      contractAddress: '0xeB510b6a149d9e18094dc7bAD1714d27eCC3B17a',
+      functionName: 'safeMint',
+      abi: abi,
+      msgValue: Moralis.Units.ETH('0.01'),
+    };
+    // add ether to in sendOption for MOralis
+    const transaction = await Moralis.executeFunction(sendOptions);
+  };
+
+  // Create a function to get total Supply of our NFT
+  // how many nft have been minted so far
+  // use the totalSupply function to get totalSupply()
+  const getTotalSupply = async () => {
+    const sendOptions = {
+      contractAddress: '0xeB510b6a149d9e18094dc7bAD1714d27eCC3B17a',
+      functionName: 'totalSupply',
+      abi: abi,
+    };
+    const message = await Moralis.executeFunction(sendOptions);
+    setTotalSupply(message.toNumber());
+  };
+
+  const login = async () => {
+    if (!isAuthenticated) {
+      await authenticate({ signingMessage: 'Log in using Moralis' })
+        .then(function (user) {
+          console.log('logged in user:', user);
+          console.log(user.get('ethAddress'));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <div className="App">
       <video
-        class="backgroundvideo"
-        src="https://static.videezy.com/system/resources/previews/000/043/267/original/4K_14.mp4"
+        className="background-video"
+        // src="https://static.videezy.com/system/resources/previews/000/043/267/original/4K_14.mp4"
+        src="https://cdn-std.droplr.net/files/acc_990213/aIieHW"
+        type="video/mp4"
         width="500"
         height="300"
         autoPlay
@@ -15,7 +83,7 @@ function App() {
         loop
       ></video>
       <div className="main">
-        <div className="mintingSection">
+        <div className="minting-section">
           <div className="left">
             <video height="400" width="400" autoPlay muted playsInline loop>
               <source
@@ -27,9 +95,22 @@ function App() {
           </div>
           <div className="right">
             <h2> Addicted: Into the Metaworse </h2>
-            <div className="info-text">21 minted / 420</div>
+            <div className="info-text">{totalSupply} minted / 420</div>
             <div className="actions">
-              <div className="minting-Btn">CONNECT WALLET</div>
+              {isAuthenticated ? (
+                <div className="mintStart">
+                  <div onClick={mint} className="wallet-btn">
+                    MINT
+                  </div>
+                  <div onClick={logOut} className="wallet-btn">
+                    START OVER
+                  </div>
+                </div>
+              ) : (
+                <div onClick={login} className="wallet-btn">
+                  CONNECT WALLET
+                </div>
+              )}
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MoralisProvider
+    serverUrl="https://wtza9eh7huca.usemoralis.com:2053/server"
+    appId="iAxM1oK6PvMRPTH9QXa36N5ZPeh2jEH8EHyPMKz9"
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MoralisProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
